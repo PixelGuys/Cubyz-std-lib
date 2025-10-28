7a890fe855e4b0cc3c82c5bc12e58e462ce2c928
IntegratedQuantum <jahe788@gmail.com>
2025-10-28 11:18:15 +0100

    Apply changes to the test runner

--- lib/compiler/test_runner.zig
+++ lib/compiler/test_runner.zig
@@ -202,7 +202,6 @@ fn mainServer() !void {
                     },
                 };
                 if (!is_fuzz_test) @panic("missed call to std.testing.fuzz");
-                if (log_err_count != 0) @panic("error logs detected");
                 assert(mode != .forever);
                 std.process.exit(0);
             },
@@ -228,7 +227,11 @@ fn mainTerminal() void {
         .root_name = "Test",
         .estimated_total_items = test_fn_list.len,
     });
-    const have_tty = std.fs.File.stderr().isTty();
+    const doColors = std.fs.File.stderr().supportsAnsiEscapeCodes();
+    const reset = if (doColors) "\x1b[0m" else "";
+    const red = if (doColors) "\x1b[31m" else "";
+    const yellow = if (doColors) "\x1b[33m" else "";
+    const green = if (doColors) "\x1b[32m" else "";
 
     var leaks: usize = 0;
     for (test_fn_list, 0..) |test_fn, i| {
@@ -241,33 +244,22 @@ fn mainTerminal() void {
         testing.log_level = .warn;
 
         const test_node = root_node.start(test_fn.name, 0);
-        if (!have_tty) {
-            std.debug.print("{d}/{d} {s}...", .{ i + 1, test_fn_list.len, test_fn.name });
-        }
+
+        std.debug.print("{d: >4}/{d: <4} {s:.<65}", .{ i + 1, test_fn_list.len, test_fn.name });
         is_fuzz_test = false;
         if (test_fn.func()) |_| {
             ok_count += 1;
             test_node.end();
-            if (!have_tty) std.debug.print("OK\n", .{});
+            std.debug.print("{s}OK{s}\n", .{ green, reset });
         } else |err| switch (err) {
             error.SkipZigTest => {
                 skip_count += 1;
-                if (have_tty) {
-                    std.debug.print("{d}/{d} {s}...SKIP\n", .{ i + 1, test_fn_list.len, test_fn.name });
-                } else {
-                    std.debug.print("SKIP\n", .{});
-                }
+                std.debug.print("{s}SKIP{s}\n", .{ yellow, reset });
                 test_node.end();
             },
             else => {
                 fail_count += 1;
-                if (have_tty) {
-                    std.debug.print("{d}/{d} {s}...FAIL ({t})\n", .{
-                        i + 1, test_fn_list.len, test_fn.name, err,
-                    });
-                } else {
-                    std.debug.print("FAIL ({t})\n", .{err});
-                }
+                std.debug.print("{s}FAIL{s}\n{s}:\n", .{ red, reset, @errorName(err) });
                 if (@errorReturnTrace()) |trace| {
                     std.debug.dumpStackTrace(trace);
                 }
@@ -291,7 +283,7 @@ fn mainTerminal() void {
     if (fuzz_count != 0) {
         std.debug.print("{d} fuzz tests found.\n", .{fuzz_count});
     }
-    if (leaks != 0 or log_err_count != 0 or fail_count != 0) {
+    if (leaks != 0 or fail_count != 0) {
         std.process.exit(1);
     }
 }
